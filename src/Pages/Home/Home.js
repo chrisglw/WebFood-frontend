905ce7273aba4bcd06ea3d7df1f11336f39a26bb
christian <lan21011@byui.edu>
2024-11-12 20:55:31 -0700

    HomePage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,6 +8,10 @@ function Home() {
             <div className="hero-image"></div>
             <div className="content-below-hero">
                 <h2>Dine-in 11-3pm. Pick-Up & Delivery until 9:30 p.m.</h2>
+                <h3>
+                    Explore our <Link to="/menu" className="menu-link">Menu,</Link> then order by phone or place your order online through{' '}
+                    <a href="https://www.grubhub.com" className="grubhub-link">GRUBHUB</a>.
+                </h3>
                 <Link to="/menu">
                     <button className="menu-button">Menu</button>
                 </Link>
